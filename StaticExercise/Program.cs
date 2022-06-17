6423e7e51e7ad32fc2d630f71a29c6f1a30e8dd8
JRS80 <jason.steffen@gmail.com>
2022-06-17 13:33:00 -0500

    module 8 static exercises

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+
+        public static class TempConverter
+        {
+            public static double FahrenheitToCelsius(double f) => (f - 32) * (5 / 9);
+            public static double CelsiusToFahrenheit(double c) => c * (9 / 5) + 32;
+        }
+
+        static void Main(string[] args)
+        {
+            double temp;
+            Console.WriteLine("Enter temperature in F: ");
+            temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{temp} degrees F is equivalent to {TempConverter.FahrenheitToCelsius(temp)} degrees C.");
+            Console.ReadKey(true);
+            Console.WriteLine("\nNow enter a temperature in C: ");
+            temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{temp} degrees C is equivalent to {TempConverter.CelsiusToFahrenheit(temp)} degrees F.");
+            Console.ReadKey(true);
+        }
+    }
+}
